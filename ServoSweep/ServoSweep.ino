6d34e49e89fc2a14f934009bfe4a67ebb40ceca6
Marcio Lima <mdlima.github@mailnull.com>
2012-08-28 17:27:50 -0300

    Do the sweep using pulse length in microseconds.

--- ServoSweep/ServoSweep.ino
+++ ServoSweep/ServoSweep.ino
@@ -7,29 +7,47 @@
  
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
+Servo myServo2;
  
 int pos = 0;    // variable to store the servo position 
  
 void setup() 
 { 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
-//  myservo.write(90);
+  myservo.attach(4);  // attaches the servo on pin 9 to the servo object 
+  myServo2.attach(5);
+  myservo.write(90);
+  myServo2.write(90);
 } 
  
  
 void loop() 
 { 
 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+//  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+//  {                                  // in steps of 1 degree 
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//    delay(15);                       // waits 15ms for the servo to reach the position 
+//  } 
+//  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+//  {                                
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//    delay(15);                       // waits 15ms for the servo to reach the position 
+//  }
+
+  for(pos = MIN_PULSE_WIDTH; pos <= MAX_PULSE_WIDTH; pos += 1)  // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
+    myservo.writeMicroseconds(pos);              // tell servo to go to position in variable 'pos' 
+    delay(1);                       // waits 15ms for the servo to reach the position 
   } 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  for(pos = MAX_PULSE_WIDTH; pos>=MIN_PULSE_WIDTH; pos-=1)     // goes from 180 degrees to 0 degrees 
   {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
+    myservo.writeMicroseconds(pos);              // tell servo to go to position in variable 'pos' 
+    delay(1);                       // waits 15ms for the servo to reach the position 
   } 
 
+//  myservo.write(0);
+//  delay(2700);
+//  myservo.write(180);
+//  delay(2700);
 
 } 
